05e90b5fbff336b92696a9ca63f763ef74af7cc1
catiulll <satriafzal@gmail.com>
2026-06-26 23:11:12 +0700

    update app vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <div class="min-h-screen bg-dark-purple text-white font-sans overflow-x-hidden">
     <Navbar/>
     <RouterView />
+    <Footer/>
   </div>
 </template>
 
