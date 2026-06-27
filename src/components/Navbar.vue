d5157d2735576e8eafba39bbbcdb18765faecc79
catiulll <satriafzal@gmail.com>
2026-06-27 21:34:36 +0700

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,8 @@
         <span class="text-transparent bg-clip-text bg-gradient-to-r from-neon-cyan to-neon-purple">ECO</span>PULSE
       </router-link>
       
-      <nav v-if="!authpage" class="hidden md:flex gap-8 text-sm font-medium">
+      <!-- Kalau bukan di halaman hideMenu, tampilkan link navigasi -->
+      <nav v-if="!hideMenu" class="hidden md:flex gap-8 text-sm font-medium">
         <a href="#about" class="hover:text-neon-cyan transition-colors duration-300">About</a>
         <a href="#live-data" class="hover:text-neon-cyan transition-colors duration-300">Live Data</a>
         <a href="#aqi-map" class="hover:text-neon-cyan transition-colors duration-300">AQI Map</a>
@@ -13,18 +14,18 @@
       </nav>
 
       <div class="flex gap-6 items-center">
-
-        <template v-if="!authpage">
+        <!-- Kalau bukan di halaman hideMenu, tampilkan Login/Register -->
+        <template v-if="!hideMenu">
             <router-link to="/login" class="text-sm font-bold hover:text-neon-cyan transition-colors duration-300">LOGIN</router-link>
             <router-link to="/register" class="px-7 py-2 text-sm bg-gradient-to-r from-neon-cyan to-neon-purple rounded-full font-bold text-dark-purple hover:scale-105 transition-transform duration-300">REGISTER</router-link>
         </template>
 
+        <!-- Kalau SEDANG di halaman hideMenu, tampilkan tombol Kembali -->
         <template v-else>
             <RouterLink to="/" class="px-6 py-2 text-sm border-2 border-dark-border hover:border-neon-cyan rounded-full font-bold text-gray-300 hover:text-white transition-all duration-300 flex items-center gap-2">
                 <i class="pi pi-arrow-left text-xs"></i> Kembali
             </RouterLink>
         </template>
-
       </div>
     </header>
 </template>
@@ -39,6 +40,5 @@
       window.scrollTo({ top: 0, behavior: 'smooth'});
     }
 
-    const authpage = computed(() => ['/login', '/register'].includes(route.path));
-
+    const hideMenu = computed(() => ['/login', '/register', '/pusat-bantuan'].includes(route.path));
 </script>
