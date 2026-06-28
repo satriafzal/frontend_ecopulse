f3c153de403570f70f9c6cfe79eaed8fcaa30623
catiulll <satriafzal@gmail.com>
2026-06-28 22:13:35 +0700

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import HomeView from '../views/HomeView.vue'
 import Login from '@/views/auth/Login.vue'
 import Register from '@/views/auth/Register.vue'
 import Helpcenter from '@/views/Helpcenter.vue'
+import Privacy from '@/views/Privacy.vue'
+import Syarat_ketentuan from '@/views/Syarat_ketentuan.vue'
+import Dashboarduser from '@/views/users/Dashboarduser.vue'
+import Profileuser from '@/views/users/Profileuser.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +30,32 @@ const router = createRouter({
       path: '/pusat-bantuan',
       name: 'pusatbantuan',
       component: Helpcenter
+    },
+    {
+      path: '/privasi',
+      name: 'kebijakan-privasi',
+      component: Privacy
+    },
+    {
+      path: '/syarat_ketentuan',
+      name: 'syarat-dan-ketentuan',
+      component: Syarat_ketentuan
+    },
+    {
+      path: '/user',
+      component: () => import('@/views/users/DashboardLayout.vue'),
+      children: [
+          {
+          path: 'db_user',
+          name: 'user-dashboard',
+          component: Dashboarduser
+        },
+        {
+          path: 'pf_user',
+          name: 'profile-user',
+          component: Profileuser
+        }
+      ]
     },
   ],
   scrollBehavior(to, from, savedPosition) {
