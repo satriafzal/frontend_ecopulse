41d17a7c92e41a66ce7e2a2a3a2affbf4bb94cec
catiulll <satriafzal@gmail.com>
2026-06-28 22:12:37 +0700

    update app vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
+import { watch, onMounted, computed} from 'vue'
+import AOS from 'aos'
+
+const route = useRoute();
+
+const isUserArea = computed(() => {
+  return route.matched.some(record => record.path === '/user')
+})
+
+// inisialisasi aos
+onMounted(() => {
+  AOS.init({
+    once: true,      
+    offset: 100,     
+    duration: 1500,  
+    easing: 'ease-out-cubic',
+  })
+})
 </script>
 
 <template>
   <div class="min-h-screen bg-dark-purple text-white font-sans overflow-x-hidden">
-    <Navbar/>
+    <Navbar v-if="!isUserArea"/>
     <RouterView />
-    <Footer/>
+    <Footer v-if="!isUserArea"/>
   </div>
 </template>
 
