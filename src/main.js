f182316165677985603830e3ecc5c33dcb406ffc
catiulll <satriafzal@gmail.com>
2026-07-08 23:39:32 +0700

    update main js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import App from './App.vue'
 import router from './router'
 import 'primeicons/primeicons.css'
 import './assets/main.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import ToastService from 'primevue/toastservice'
 
 // for aos style
 import 'aos/dist/aos.css'
@@ -14,7 +17,22 @@ import 'aos/dist/aos.css'
 
 const app = createApp(App)
 
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: 'system',
+            cssLayer: {
+                name: 'primevue',
+                order: 'tailwind-base, primevue, tailwind-utilities'
+            }
+        }
+    }
+})
+
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
+app.use(ToastService)
 
 app.mount('#app')
